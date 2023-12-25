9cc0ebd52cb6c008d0f3409a956c1e059a08a31a
SammriddhGupta <sammriddhgupta@gmail.com>
2023-12-25 16:04:45 +1100

    add feels-like

--- script.js
+++ script.js
@@ -19,12 +19,14 @@ let weather = {
     displayWeather: function(data) {
         const { name } = data;
         const { icon, description } = data.weather[0];
-        const { temp, humidity } = data.main;
+        const { temp, feels_like, humidity } = data.main;
+        // add feels-like 
         const { speed }  = data.wind;
         document.querySelector(".city").innerText = "Weather in "+name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".feels-like").innerText = "Feels Like: " + feels_like + "°C";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/hr";
         document.querySelector(".weather").classList.remove("loading");
